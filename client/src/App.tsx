bdb276a9e6f2d61fb394a78149d09978602ca3a4
drewbs <p3nd3rs@gmail.com>
2026-03-21 16:27:49 +0000

    feat: add CSS modules and reusable boilerplate from foundations vault

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
-import './App.css'
-
 function App() {
   return (
     <div className="app">
       <h1>HDR UK Dataset Explorer</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
